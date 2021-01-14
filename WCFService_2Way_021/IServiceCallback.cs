0516ee3c82a568b48932d2407e64070066f6e553
DimasBagasAjipratama <56101230+DimasBagasAjipratama@users.noreply.github.com>
2021-01-14 22:25:33 +0700

    Membuat service pesan

--- WCFService_2Way_021/IServiceCallback.cs
+++ WCFService_2Way_021/IServiceCallback.cs
@@ -8,20 +8,20 @@ using System.Text;
 namespace WCFService_2Way_021
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-    [ServiceContract]
-    public interface IService1
+    [ServiceContract(CallbackContract = typeof(IClientCallback))]
+    public interface IServiceCallback
     {
-        [OperationContract]
-        string GetData(int value);
+        [OperationContract(IsOneWay = true)]
+        void gabung(string username);
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        [OperationContract(IsOneWay = true)]
+        void kirimPesan(string pesan);
 
         // TODO: Add your service operations here
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WCFService_2Way_021.ContractType".
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WCFService_2Way_027.ContractType".
     [DataContract]
     public class CompositeType
     {
